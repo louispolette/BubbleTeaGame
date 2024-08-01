fa197161aeb32712b942a322065177d81ba25da1
Louis Polette <lpolette2@gmail.com>
2024-08-01 18:37:41 +0200

    Fillable Cups

---
+++ Assets/_Scripts/FX/CupFill.cs
@@ -0,0 +1,120 @@
+using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CupFill : MonoBehaviour
+{
+    [Space]
+
+    [SerializeField, Range(0f, 1f)] private float _fillRatio = 1f;
+
+    [Space]
+
+    [SerializeField] private ScaledObject[] _objectsToScale;
+
+    [Space]
+
+    [SerializeField] private Renderer[] _renderersToHide;
+
+    [Space]
+
+    [SerializeField] private bool _updateEveryFrame = false;
+
+    private bool _renderersEnabled;
+
+    private void Awake()
+    {
+        _renderersEnabled = _fillRatio > 0f;
+    }
+
+    private void Update()
+    {
+        if (!_updateEveryFrame) return;
+
+        UpdateObjects();
+        CheckIfEmpty();
+    }
+
+    private void UpdateObjects()
+    {
+        foreach (ScaledObject obj in _objectsToScale)
+        {
+            if (obj.affectScale)
+            {
+                float width = Mathf.Lerp(obj.emptyScale.x, obj.fullScale.x, _fillRatio);
+                float height = Mathf.Lerp(obj.emptyScale.y, obj.fullScale.y, _fillRatio);
+
+                obj.objectTransform.localScale = new Vector3(width, height, obj.objectTransform.localScale.z);
+            }
+            if (obj.affectPosition)
+            {
+                float xPos = Mathf.Lerp(obj.emptyPosition.x, obj.fullPosition.x, _fillRatio);
+                float yPos = Mathf.Lerp(obj.emptyPosition.y, obj.fullPosition.y, _fillRatio);
+
+                obj.objectTransform.localPosition = new Vector3(xPos, yPos, obj.objectTransform.localPosition.z);
+            }
+        }
+    }
+
+    private void CheckIfEmpty()
+    {
+        if (_fillRatio > 0f)
+        {
+            EnableRenderers();
+        }
+        else
+        {
+            Debug.Log("Disable");
+            DisableRenderers();
+        }
+    }
+
+    private void DisableRenderers()
+    {
+        if (!_renderersEnabled) return;
+
+        foreach(Renderer renderer in _renderersToHide)
+        {
+            renderer.enabled = false;
+        }
+
+        _renderersEnabled = false;
+    }
+
+    private void EnableRenderers()
+    {
+        if (_renderersEnabled) return;
+
+        foreach (Renderer renderer in _renderersToHide)
+        {
+            renderer.enabled = true;
+        }
+
+        _renderersEnabled = true;
+    }
+
+    public void SetFillRatio(float ratio)
+    {
+        _fillRatio = Mathf.Clamp01(ratio);
+        UpdateObjects();
+        CheckIfEmpty();
+    }
+
+    [Serializable]
+    private struct ScaledObject
+    {
+        public Transform objectTransform;
+
+        [Space]
+
+        public bool affectPosition;
+        public Vector2 emptyPosition;
+        public Vector2 fullPosition;
+
+        [Space]
+
+        public bool affectScale;
+        public Vector2 emptyScale;
+        public Vector2 fullScale;
+    }
+}
---
+++ Assets/_Scripts/Items/CookableObject.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public abstract class CookableObject : MonoBehaviour
+{
+
+}
